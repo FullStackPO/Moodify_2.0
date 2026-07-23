91f79d7f85ce28ff902dfc14abfd1cede0801458
Harsh Arya <harsharya8946@gmail.com>
2026-07-23 23:49:14 +0530

    songs upload API

--- server/src/controllers/song.controller.js
+++ server/src/controllers/song.controller.js
@@ -1,8 +1,39 @@
 const songModel = require('../models/song.model')
+const storageService = require('../services/storage.service')
+const id3 = require('node-id3')
 
 async function uploadSong(req, res){
 
-    console.log(req.file)
+    const songBuffer = req.file.buffer
+
+    const tags = id3.read(songBuffer)
+
+    console.log(tags)
+
+    const songFile = await storageService.uploadFile({
+        buffer : songBuffer,
+        filename : tags.title + ".mp3",
+        folder : "/moodify_2.0"
+    })
+
+    const posterFile = await storageService.uploadFile({
+        buffer : tags.image.imageBuffer,
+        filename : tags.title + ".jpeg",
+        folder : "/moodify_2.0"
+    })
+
+    const song = await songModel.create({
+        url : songFile.url,
+        posterUrl : posterFile.url,
+        title : tags.title,
+        mood
+    })
+
+    res.status(201).json({
+        message : 'song added successfully',
+        song
+    })
+
 
 }
 
--- server/src/models/song.model.js
+++ server/src/models/song.model.js
@@ -13,6 +13,12 @@ const songSchema = new mongoose.Schema({
     title : { 
         type : String,
         required : true
+    },
+    mood:{
+        type : String,
+        enum : {
+            values : ["happy", "sad", "surprised"]
+        } 
     }
 
 })
